e30008f3c3effa6585773d50caee1b6c1a44b658
Jay Em <jaybacs4@gmail.com>
2025-04-05 17:33:53 +0800

    trial commit

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	export let name;
+	export let name = "Jay EM";
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p style="red">Long Live Master{name}</p>
 </main>
 
 <style>
